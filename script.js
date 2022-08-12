deb224fdb1f851f99aafeba92d2dbd2d37a6b161
Yuri <worltoffi@gmail.com>
2022-08-12 10:54:52 +0300

    my first new  commi

---
+++ script.js
@@ -0,0 +1,69 @@
+
+
+
+let userName = "Jorh";
+let userNumember = 25;
+
+console.log(userName);
+
+const obj = {
+  name: "Yuri",
+  age: 25,
+  isMarried: false, 
+};
+
+console.log(obj.name);
+// console.log(obj.["name"]);
+
+let arr = ['plum.png', 'yurik.svg', 6, {}, [],'huut.mp3'];
+
+console.log(arr[0]);
+
+const arrei = [1, 2, 3,];
+
+const arrrr = ['a', 'b', 'c'];
+const arrObj = {
+  0:'a',
+  1:'b',
+  2:'c',
+};
+
+// const result = confirm('Are you here?');
+// const ansem = prompt("Вам есть 18 лет?","18");
+
+// console.log(ansem + 5);
+
+// function addTime () {
+//   let humm = 10;
+//   let afff = 15;
+
+//   document.getElementById('ippApp').onclick = Fusddd;
+//   return;
+// };
+
+// const answers = [];
+
+// answers[0] = prompt("Как Вас зовут?", "");
+// answers[1] = prompt("Где живете?", "");
+// answers[2] = prompt("Сколько лет?", "");
+
+// document.write(answers);
+// console.log(typeof (answers));
+
+let inner = 10,
+    deinnr = 10;
+
+inner++;
+deinnr--;
+
+console.log(inner);
+console.log(deinnr);
+
+console.log(5%2);
+
+console.log(2*4 === "8");
+
+const isCheked = true,
+      isCheartu = false;
+
+console.log(isCheked && !isCheartu);
