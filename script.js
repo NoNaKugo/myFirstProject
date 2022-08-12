03fbd39bed94fb23a0fcb5c2846bc20b70490689
Yuri <worltoffi@gmail.com>
2022-08-12 15:47:53 +0300

    Сделал задания :)

--- script.js
+++ script.js
@@ -1,69 +1,46 @@
+/* Задание на урок:
 
+1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
+'Сколько фильмов вы уже посмотрели?'
 
+2) Создать объект personalMovieDB и в него поместить такие свойства:
+    - count - сюда передается ответ на первый вопрос
+    - movies - в это свойство поместить пустой объект
+    - actors - тоже поместить пустой объект
+    - genres - сюда поместить пустой массив
+    - privat - в это свойство поместить boolean(логическое) значение false
 
-let userName = "Jorh";
-let userNumember = 25;
+3) Задайте пользователю по два раза вопросы:
+    - 'Один из последних просмотренных фильмов?'
+    - 'На сколько оцените его?'
+Ответы стоит поместить в отдельные переменные
+Записать ответы в объект movies в формате: 
+    movies: {
+        'logan': '8.1'
+    }
 
-console.log(userName);
+Проверить, чтобы все работало без ошибок в консоли */
 
-const obj = {
-  name: "Yuri",
-  age: 25,
-  isMarried: false, 
+'use strict';
+ 
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count:  numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
 };
 
-console.log(obj.name);
-// console.log(obj.["name"]);
+const a = prompt('Один из последних просмотренных фильмов?',''),
+      b = prompt('На сколько оцените его?',''),
+      c = prompt('Один из последних просмотренных фильмов?',''),
+      d = prompt('На сколько оцените его?','');
 
-let arr = ['plum.png', 'yurik.svg', 6, {}, [],'huut.mp3'];
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-console.log(arr[0]);
+console.log(personalMovieDB);
 
-const arrei = [1, 2, 3,];
 
-const arrrr = ['a', 'b', 'c'];
-const arrObj = {
-  0:'a',
-  1:'b',
-  2:'c',
-};
-
-// const result = confirm('Are you here?');
-// const ansem = prompt("Вам есть 18 лет?","18");
-
-// console.log(ansem + 5);
-
-// function addTime () {
-//   let humm = 10;
-//   let afff = 15;
-
-//   document.getElementById('ippApp').onclick = Fusddd;
-//   return;
-// };
-
-// const answers = [];
-
-// answers[0] = prompt("Как Вас зовут?", "");
-// answers[1] = prompt("Где живете?", "");
-// answers[2] = prompt("Сколько лет?", "");
-
-// document.write(answers);
-// console.log(typeof (answers));
-
-let inner = 10,
-    deinnr = 10;
-
-inner++;
-deinnr--;
-
-console.log(inner);
-console.log(deinnr);
-
-console.log(5%2);
-
-console.log(2*4 === "8");
-
-const isCheked = true,
-      isCheartu = false;
-
-console.log(isCheked && !isCheartu);
